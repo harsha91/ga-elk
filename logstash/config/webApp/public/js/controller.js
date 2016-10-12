17c671bb1049ad3a308a5b763fa2b0456bfd8484
harsha91 <s.harsha.sri@gmail.com>
2016-10-11 19:08:12 -0600

    more changes

--- logstash/config/webApp/public/js/controller.js
+++ logstash/config/webApp/public/js/controller.js
@@ -7,20 +7,6 @@ gaApp.config(['$routeProvider',function($routeProvider) {
                 templateUrl : 'pages/home.html',
                 controller  : 'mainController'
             })
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
-            // route for the data page
-            .when('/data', {
-                templateUrl : 'pages/table.html',
-                controller  : 'tabController'
-            })
-            .when('/tabData', {
-                templateUrl : 'pages/tabData.html',
-                controller  : 'tableDController'
-            });
     }]);
 gaApp.directive("repeatEnd", function(){
             return {
@@ -57,79 +43,3 @@ function allowPatternDirective() {
 gaApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
     $scope.pageClass = 'page-home';
 }]);
-
-gaApp.controller('aboutController', function($scope) {
-    $scope.pageClass = 'page-about';
-});
-
-gaApp.controller('tabController',function($scope,$http){
-    $http.get("api/getTables").success(function(response){ 
-        $scope.tables = response;  
-    }); 
-    var progress = 0;
-    $scope.colS =function(){
-        if(progress < 5){
-          progress++;
-          $http.get("api/getTablesC?col="+$scope.search).success(function(response){ 
-            $scope.tables = response;
-            progress--;
-          }).error(function(){progress = 0});  
-        } 
-    }
-});
-gaApp.controller('tableDController',function($scope,$http,$routeParams,$timeout ){
-    var tabName = $routeParams.tab;
-    var from = $routeParams.from;
-    if(typeof(from) === "undefined" || from == null || from === ''){
-        from=0
-    }
-    $scope.loading=true;
-    var to = parseInt(from)+100;
-    $scope.tabName = tabName;    
-    if(typeof(tabName)!== "undefined"){
-        $http.get("api/getData?tab="+tabName+"&from="+from+"&to=100").success(function(response){
-        $scope.headers = response.data[1];
-        $scope.tabData = response.data;
-        $scope.totRecords = response.totalRecords;
-        $scope.from = response.from;
-        $scope.to = parseInt(response.from) + parseInt(response.fecthedRecords);
-    }).error(function(){
-            $scope.loading=false;
-        });
-    var table = null;
-    $scope.onEnd = function(){
-                $timeout(function(){
-                 table  =  $( "#tabData" ).DataTable({
-                        "scrollY": 300,
-                        "scrollX": true,
-                        "paging":false
-                       // "repsonsive":true
-                    });
-                    $scope.loading=false;
-                }, 1);
-            };
-    }else{
-        $scope.loading=false;
-    };
-    $http.get("api/getTables").success(function(response){ 
-        $scope.tables = response;  //ajax request to fetch data into $scope.data
-    });
-    var button = angular.element(document.getElementsByClassName('offTabOpen'));
-    var menu = angular.element(document.getElementsByClassName('offTab'));
-    var overlay = angular.element(document.getElementsByClassName('overlay'));
-    $scope.toggleMenu =function(){
-        button.toggleClass('op');
-        menu.toggleClass('open');
-        overlay.toggleClass('oHide');
-    };  
-    var progress = 0;
-    $scope.colS =function(){
-        if(progress < 5){
-          progress++;
-          $http.get("api/getTablesC?col="+$scope.search).success(function(response){ 
-            $scope.tables = response;
-            progress--;
-          }).error(function(){progress = 0});  
-        } 
-    }
-});
