04a61b817f669b756e22d2f2a7e5b9e6741cdf38
harsha91 <s.harsha.sri@gmail.com>
2016-11-04 11:04:33 -0600

    aws deploy configurations

--- prodDeploy.sh
+++ prodDeploy.sh
@@ -1,5 +1,5 @@
 # Creates Cluster
-ecs-cli up --keypair harsha --capability-iam --size 2 --instance-type t2.medium
+ecs-cli up --keypair harsha --capability-iam --size 1 --instance-type t2.medium
 
 # Deploys the containers
 ecs-cli compose --file docker-compose-p.yml up
